ff1271a0b1d1652e2af5f6354ddbaf37925abfe4
lynn2910 <cedric.colin35@gmail.com>
2025-05-01 00:31:36 +0200

    Updates

--- _app/immutable/chunks/DojzkUpX.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1eeora6)==null?void 0:s.base)??"/Svelte-TodoApp";var e;const t=((e=globalThis.__sveltekit_1eeora6)==null?void 0:e.assets)??a;export{t as a,a as b};
---
+++ _app/immutable/chunks/UErh89_K.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_61t16c)==null?void 0:s.base)??"/Svelte-TodoApp";var t;const a=((t=globalThis.__sveltekit_61t16c)==null?void 0:t.assets)??e;export{a,e as b};
