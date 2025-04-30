794ef57132e9a0ba16f43eeeaa268ead04384add
lynn2910 <cedric.colin35@gmail.com>
2025-05-01 00:40:08 +0200

    Updates

---
+++ _app/immutable/chunks/B7DNMO60.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_qj5r8z)==null?void 0:s.base)??"/Svelte-TodoApp";var e;const a=((e=globalThis.__sveltekit_qj5r8z)==null?void 0:e.assets)??t;export{a,t as b};
--- _app/immutable/entry/start.BaZ0_RHx.js
+++
@@ -1,1 +0,0 @@
-import{l as o,d as r}from"../chunks/ClrSO2S2.js";export{o as load_css,r as start};
